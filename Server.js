ebd33cbd41617d0774b28a52115f83d0d163d7ab
MahendraRajput123 <mahendra@ealphabits.com>
2024-06-19 14:14:23 +0530

    Added 2000 port

--- Server.js
+++ Server.js
@@ -64,6 +64,10 @@ io.on("connection", (socket) => {
 
 });
 
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World!" });
+});
+
 const PORT = 2000; // or any other port you prefer
 server.listen(PORT, () => {
   console.log(`Server is running on https://localhost:${PORT}`);
